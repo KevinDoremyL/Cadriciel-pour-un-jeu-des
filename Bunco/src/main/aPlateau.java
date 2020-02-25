1a909ebf6a690394bdb5b5cc5f1c5f7344299df2
masterdoremy <kevin.doremy.l@gmail.com>
2020-02-25 15:48:44 -0500

    q

--- Bunco/src/main/aPlateau.java
+++ Bunco/src/main/aPlateau.java
@@ -2,6 +2,6 @@ public class aPlateau {
     //
     public static void main(String[] args) {
         // TODO Vue de l'app ( M V C )
-        System.out.println("Le plateau de jeu");
+        System.out.println("Le plateau de jeu!");
     }
 }
