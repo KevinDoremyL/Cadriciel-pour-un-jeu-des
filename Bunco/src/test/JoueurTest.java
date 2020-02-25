986b7dcb9d5d0372e33d3cbddbc9276c8c881540
masterdoremy <kevin.doremy.l@gmail.com>
2020-02-25 13:02:22 -0500

    Rajout d'une liste ToDo dans la classe JoueurTest

--- Bunco/src/test/JoueurTest.java
+++ Bunco/src/test/JoueurTest.java
@@ -1,12 +1,12 @@
 public class JoueurTest {
 
     // TODO Test Unitaire 2 : Dans le cas d'obtention de 0 point si le joueur passe
-    // la
-    // main au joueur suivant
+    // la main au joueur suivant
 
     // TODO Test Unitaire 3 : Dans le cas d'obtention d'un seul chiffre
     // correspondant au numéro de tour si le score est correct et si le joueur garde
     // la main.
+
     // TODO Test Unitaire 4 : Dans le cas où un joueur obtient trois chiffres
     // identiques et égaux au numéro du tour, le score calculé est 21 points et si
     // le joueur passe la main au joueur suivant.
