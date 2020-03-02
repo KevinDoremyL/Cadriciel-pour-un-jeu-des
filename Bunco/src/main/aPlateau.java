db05bbd9cbbcfae5c36fa52206f6ca627cd90c00
MrRyado <ryado@Ares.localdomain>
2020-03-02 11:15:26 -0500

    test2 ryad

--- Bunco/src/main/aPlateau.java
+++ Bunco/src/main/aPlateau.java
@@ -3,6 +3,5 @@ public class aPlateau {
     public static void main(String[] args) {
         // TODO Vue de l'app ( M V C )
         System.out.println("Le plateau de jeu!");
-        System.out.println("test ryad");
     }
 }
