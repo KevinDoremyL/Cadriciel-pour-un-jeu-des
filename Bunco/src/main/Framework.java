66ee5a6dcf6e36fa11d885aed50a9b0dae8f356a
camilechaoutene <camile.chaoutene.1@ens.etsmtl.ca>
2020-03-03 16:17:18 -0500

    testtttt

--- Bunco/src/main/Framework.java
+++ Bunco/src/main/Framework.java
@@ -10,7 +10,7 @@ public abstract class Framework {
         debutPartie();
         jouerTour();//test
         finPartie();// test ryad
-
+        // test
     }
 
     public void debutPartie() {}
