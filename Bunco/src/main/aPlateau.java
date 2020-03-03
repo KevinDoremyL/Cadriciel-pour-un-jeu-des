9f403fbac40c25e66c069ea331dd208255c31853
masterdoremy <kevin.doremy.l@gmail.com>
2020-03-03 16:02:33 -0500

    first Commit Kevin

--- Bunco/src/main/aPlateau.java
+++ Bunco/src/main/aPlateau.java
@@ -3,5 +3,6 @@ public class aPlateau {
     public static void main(String[] args) {
         // TODO Vue de l'app ( M V C )
         System.out.println("Le plateau de jeu!");
+        System.out.println("Kevin First Commit");
     }
 }
