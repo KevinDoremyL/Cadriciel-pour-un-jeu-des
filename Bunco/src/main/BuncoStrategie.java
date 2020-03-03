957e78b9e59e2e6d39115aeaeb36e5c1f86b9bb6
came1996ets <camilechaoutene@MacBook-Pro-de-Camile.local>
2020-03-03 14:05:02 -0500

    StrategieBunco

---
+++ Bunco/src/main/BuncoStrategie.java
@@ -0,0 +1,12 @@
+package main;
+
+public class BuncoStrategie implements IStrategie {
+    @Override
+    public void calculerLeVainqueur(Jeu jeu){
+
+    }
+    @Override
+    public void calculerScoreTour(Jeu jeu){
+
+    }
+}
