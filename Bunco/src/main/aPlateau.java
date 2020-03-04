473bd339d0e152e787cd8583a2138219ddb80959
masterdoremy <kevin.doremy.l@gmail.com>
2020-03-03 23:12:49 -0500

    Kevin Commmit with terminal

--- Bunco/src/main/aPlateau.java
+++ Bunco/src/main/aPlateau.java
@@ -3,7 +3,7 @@ public class aPlateau {
     public static void main(String[] args) {
         // TODO Vue de l'app ( M V C )
         System.out.println("Le plateau de jeu!");
-        System.out.println("Kevin FFFirst Commmit 1");
+        System.out.println("Kevin Commmit with terminal ");
         System.out.println("Ryad commit du chef test 2 3 4 5");
         System.out.println("Nassim test commit");
     }
