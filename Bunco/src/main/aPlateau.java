8b2f11e2a66dfaeb3e16b048a354a76e8bea8c9e
MrRyado <ryad.meftahi@gmail.com>
2020-03-05 12:56:10 -0500

    test 9

--- Bunco/src/main/aPlateau.java
+++ Bunco/src/main/aPlateau.java
@@ -4,7 +4,7 @@ public class aPlateau {
         // TODO Vue de l'app ( M V C )
         System.out.println("Le plateau de jeu!");
         System.out.println("Kevin Commmit with terminal ");
-        System.out.println("Ryad commit du chef test 8");
+        System.out.println("Ryad commit du chef test 9");
         System.out.println("Nassim test commit");
     }
 }
