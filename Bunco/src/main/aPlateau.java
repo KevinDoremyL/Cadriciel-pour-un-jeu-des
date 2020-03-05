ee3ffc33cba9519fbca0ec92a723e17732f61467
masterdoremy <kevin.doremy.l@gmail.com>
2020-03-05 17:39:22 -0500

    Kevin Commit from Intellij

--- Bunco/src/main/aPlateau.java
+++ Bunco/src/main/aPlateau.java
@@ -3,13 +3,10 @@ public class aPlateau {
     public static void main(String[] args) {
         // TODO Vue de l'app ( M V C )
         System.out.println("Le plateau de jeu!");
-<<<<<<< HEAD
+
         System.out.println("Kevin First Commmit 1");
         System.out.println("Ryad commit du chef test 2 3 4 5");
-=======
-        System.out.println("Kevin Commmit with terminal ");
-        System.out.println("Ryad commit du chef test 10");
->>>>>>> 1de6942621aa4bce2f2468f25f75117d506af812
+
         System.out.println("Nassim test commit");
     }
 }
