f17200b729cce58885bf83bf84fa94f58914a179
camilechaoutene <camile.chaoutene.1@ens.etsmtl.ca>
2020-03-08 16:06:57 -0400

    Création des itérateurs pour les collections

--- Bunco/src/main/CollectionDes.java
+++ Bunco/src/main/CollectionDes.java
@@ -1,12 +1,50 @@
-public class CollectionDes implements ICollection<De> {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
-    public CollectionDes() {
-        //test
+public class CollectionDes implements ICollection<De> {
+    private ArrayList<De> listeDe;
+    public CollectionDes(ArrayList<De> listeDe) {
+    this.listeDe = listeDe;
     }
+
 
     @Override
-    public Iterateur<De> iterateur() {
-        // TODO Auto-generated method stub
-        return null;
+    public Iterateur getIterateur() {
+        return new IterateurDe() {
+        };
     }
+
+    class IterateurDe implements Iterateur<De> {
+        private int index=0;
+        @Override
+        public void reset() {
+            this.index=0;
+
+        }
+
+        @Override
+        public De next() {
+
+            if(this.hasNext()){
+                return listeDe.get(index++);
+            }
+            return null;
+        }
+
+        @Override
+        public De currentItem() {
+          return listeDe.get(index);
+        }
+
+        @Override
+        public boolean hasNext() {
+        
+        if(index < listeDe.size()){
+            return true;
+        }
+            return false;
+        }
+
+    }
+
 }
---
+++ Bunco/src/main/IterateurDe.java
@@ -0,0 +1,34 @@
+public class IterateurDe implements Iterateur<De> {
+    private int index;
+    @Override
+    public void reset() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public De next() {
+        // TODO Auto-generated method stub
+        // return liste.get(index++)
+        return null;
+    }
+
+    @Override
+    public De currentItem() {
+        // TODO Auto-generated method stub
+        // return liste.get(index)
+        return null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        /*
+        if(index < TailleDeLaListe){
+            return true;
+        }
+        */
+
+        return false;
+    }
+
+}
