e1b7db3c6372e12dacbe4e205900ff1784720dd2
MrRyado <ryad.meftahi@gmail.com>
2020-03-09 19:37:31 -0400

    commit terminal

--- Bunco/src/main/aPlateau.java
+++ Bunco/src/main/aPlateau.java
@@ -5,7 +5,7 @@ public class aPlateau {
         System.out.println("Le plateau de jeu!");
 
         System.out.println("Kevin First Commmit 1");
-        System.out.println("Ryad commit du chef test 2 3 4 5");
+        System.out.println("Ryad commit du chef test 2 3 4 5 6");
 
         System.out.println("Nassim test commit");
     }
