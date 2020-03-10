9a45e2b30accfe4b89f2daa524b1bcae1bd42843
masterdoremy <kevin.doremy.l@gmail.com>
2020-03-10 16:15:37 -0400

    Develop sd

--- Bunco/src/main/Fabrique.java
+++ Bunco/src/main/Fabrique.java
@@ -2,7 +2,7 @@ public class Fabrique {
     // TODO Controlleur de l'app ( M V C )
     public Fabrique() {
         // TODO new Dés Here
-        // TODO new Joeur Here
+        // TODO new Joeur Hgit ere
         // TODO new Jeu Here
     }
 
