8d795048a0b71cfd42037f635637d31945819329
nsmlkz <nassimlaar@gmail.com>
2020-03-10 13:05:03 -0400

    Joueur score et nom

--- Bunco/src/main/Joueur.java
+++ Bunco/src/main/Joueur.java
@@ -1,6 +1,31 @@
 public class Joueur implements Comparable<Joueur> {
+
+    private String nomJoueur;
+    private int scoreJoueur;
+
     public Joueur() {
 
+    }
+
+    public Joueur(String nomJoueur, int scoreJoueur) {
+        this.nomJoueur = nomJoueur;
+        this.scoreJoueur = scoreJoueur;
+    }
+
+    public String getNomJoueur() {
+        return nomJoueur;
+    }
+
+    public void setNomJoueur(String nomJoueur) {
+        this.nomJoueur = nomJoueur;
+    }
+
+    public int getScoreJoueur() {
+        return scoreJoueur;
+    }
+
+    public void setScoreJoueur(int scoreJoueur) {
+        this.scoreJoueur = scoreJoueur;
     }
 
     @Override
