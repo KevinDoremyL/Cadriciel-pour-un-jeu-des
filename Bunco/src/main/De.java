02a2c34f3f0f4d7cbdb0a159b281fbb634ffa1cd
nsmlkz <nassimlaar@gmail.com>
2020-03-10 14:15:13 -0400

    CollectionDe et De

--- Bunco/src/main/De.java
+++ Bunco/src/main/De.java
@@ -1,24 +1,29 @@
 public class De implements Comparable<De> {
-    int nbFaces;
-
-    public De() {
-
-    }
+    private int nbFaces;
 
     public De(int nbFaces) {
         this.nbFaces = nbFaces;
     }
 
-    public void setFaceObtenue(int nbFaces) {
-        this.nbFaces = nbFaces;
+    public void setFaceObtenue(int faceObtenue) {
+        this.nbFaces = faceObtenue;
     }
+    public int roll()
+    {
+        this.nbFaces = (int)(Math.random() * nbFaces) + 1;
 
-    public int getFaceObtenue() {
-        return nbFaces;
+        return this.nbFaces;
     }
     @Override
     public int compareTo(De o) {
         // TODO Auto-generated method stub
         return 0;
     }
+    @Override
+    public String toString()
+    {
+        String result = Integer.toString(this.nbFaces);
+        return result;
+    }
+
 }
