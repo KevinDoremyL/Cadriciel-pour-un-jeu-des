390225369789c97ff1b2b02c33da02d7be427727
masterdoremy <kevin.doremy.l@gmail.com>
2020-03-15 19:17:53 -0400

    Début JeuTest

--- Bunco/src/test/JeuTest.java
+++ Bunco/src/test/JeuTest.java
@@ -1,8 +1,41 @@
 public class JeuTest {
+    private static Class<Jeu> testClass;
+
+    @Test
+    public void testClass() {
+        assertEquals(Modifier.PUBLIC, testClass.getModifiers() & Modifier.PUBLIC);
+        Class<?> ancestor = testClass.getSuperclass();
+        assertNotNull(ancestor);
+
+        boolean subjectIsAncestor = false;
+        if (Framework.class == ancestor) {
+            subjectIsAncestor = true;
+        } else {
+            Class<?> secondDegreeAncestor = ancestor.getSuperclass();
+            assertNotNull(secondDegreeAncestor);
+            assertEquals(Framework.class, secondDegreeAncestor);
+        }
+
+        if (!subjectIsAncestor) {
+            fail("Framework should be a superclass");
+        }
+
+    }
 
     // TODO Test Unitaire 1 : Le classement final des joueurs avec différents
     // scores.
     // TODO Test Unitaire 4 : Dans le cas d'obtention de 3 chiffres identiques mais
     // différents du numéro de tour si le score est de 5 points et le joueur garde
     // la main.
+
+    // TODO Test Unitaire 2 : Dans le cas d'obtention de 0 point si le joueur passe
+    // la main au joueur suivant
+
+    // TODO Test Unitaire 3 : Dans le cas d'obtention d'un seul chiffre
+    // correspondant au numéro de tour si le score est correct et si le joueur garde
+    // la main.
+
+    // TODO Test Unitaire 4 : Dans le cas où un joueur obtient trois chiffres
+    // identiques et égaux au numéro du tour, le score calculé est 21 points et si
+    // le joueur passe la main au joueur suivant.
 }
--- Bunco/src/test/JoueurTest.java
+++ Bunco/src/test/JoueurTest.java
@@ -64,17 +64,7 @@ public class JoueurTest {
     @Test
     public void joueur_Constructor_String_Int_Test() {
         assertEquals(new Joueur("Junior", 6), joueur1);
+        assertEquals(new Joueur("Junior", 6), joueur1);
     }
-
-    // TODO Test Unitaire 2 : Dans le cas d'obtention de 0 point si le joueur passe
-    // la main au joueur suivant
-
-    // TODO Test Unitaire 3 : Dans le cas d'obtention d'un seul chiffre
-    // correspondant au numéro de tour si le score est correct et si le joueur garde
-    // la main.
-
-    // TODO Test Unitaire 4 : Dans le cas où un joueur obtient trois chiffres
-    // identiques et égaux au numéro du tour, le score calculé est 21 points et si
-    // le joueur passe la main au joueur suivant.
 
 }
