89699f3fa476b9c01b09dc2bbfae0c0f582d4490
MrRyado <ryad.meftahi@gmail.com>
2020-03-15 21:53:19 -0400

    seulement bunco strat

--- Bunco/src/main/aPlateau.java
+++ Bunco/src/main/aPlateau.java
@@ -6,15 +6,18 @@ public class aPlateau {
     public static void main(String[] args) {
 
         UserInterface.debutPartie();
+
         Jeu jeu = new Jeu();
         int choixStrategie = UserInterface.choixJeu();
         BuncoStrategie buncoStrategie = new BuncoStrategie();
 
         if(choixStrategie == 1){
             jeu.setStrategy(buncoStrategie);
+        } else {
+            System.out.println("Désolé, il y a seulement Bunco pour l'instant. Ce jeu vous sera imposé.");
+            jeu.setStrategy(buncoStrategie);
         }
 
-//        jeu.setStrategy(buncoStrategie); // temporaire, vrai version en haut
         jeu.getStrategy().setupGame();
 
 
